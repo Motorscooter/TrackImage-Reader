fdf8a15dab6c3ded250326cca127ecf9492f207f
Motorscooter <Sdownard88@gmail.com>
2019-03-11 20:49:51 -0400

    Update 03/11/2019
    Updated issue with Tkinter. Added method for analyzing dataframe. Created method for exporting dataframe to excel.

--- TrackImageData.py
+++ TrackImageData.py
@@ -13,10 +13,18 @@ import numpy as np
 import csv
 from tkinter import*
 from tkinter import filedialog
+import xlsxwriter
+import openpyxl
+from bokeh.plotting import figure
+from bokeh.io import push_notebook, show, output_notebook
+from bokeh.models import ColumnDataSource
+output_notebook()
+
 
 def getdirectory():
     root = Tk()
     direct = filedialog.askopenfilename(initialdir = "/", title = "Select csv file to read")
+    root.destroy()
     return direct
 
 def trajectory(direct):
@@ -34,7 +42,6 @@ def trajectory(direct):
             headercount +=1
     del csvfile
     listcount = 0
-    checking = 0
     xlist = [0]
     ylist = [0]
     xlistname = ['Time (msec)']
@@ -57,10 +64,29 @@ def trajectory(direct):
             ylistname.append(i)
         listcount += 1
     if len(xlist)==1:
-        ydf = pd.read_csv('R&D780B Cushion.csv',names = ylistname, sep = ';', header = headercount, usecols = ylist, index_col = 0)
+        ydf = pd.read_csv(direct,names = ylistname, sep = ';', header = headercount, usecols = ylist, index_col = 0)
+        return ydf
     elif len(ylist)==1:
-        xdf = pd.read_csv('R&D780B Cushion.csv',names = xlistname, sep = ';', header = headercount, usecols = xlist, index_col = 0)
+        xdf = pd.read_csv(direct,names = xlistname, sep = ';', header = headercount, usecols = xlist, index_col = 0)
+        return xdf
     else:        
-        xdf = pd.read_csv('R&D780B Cushion.csv',names = xlistname, sep = ';', header = 5, usecols = xlist, index_col = 0)
-        ydf = pd.read_csv('R&D780B Cushion.csv',names = ylistname, sep = ';', header = 5, usecols = ylist, index_col = 0)
-    return xdf, ydf
+        xdf = pd.read_csv(direct,names = xlistname, sep = ';', header = 5, usecols = xlist, index_col = 0)
+        ydf = pd.read_csv(direct,names = ylistname, sep = ';', header = 5, usecols = ylist, index_col = 0)
+        return xdf, ydf
+#Function is used to append data frame from TrackImage data into a Master Excel sheet.
+def excelReport(buildsheet,xdf,ydf):
+    result = pd.concat([xdf,ydf],sort = False, axis=1)
+    with pd.ExcelWriter('StabilityMaster.xlsx',mode = 'a',engine='openpyxl') as writer:
+        result.to_excel(writer,sheet_name=buildsheet)
+
+
+def analyze(df):
+    plot = figure()
+    src = ColumnDataSource(df)
+    srcdict = dict(src.data)
+    keylist = list(srcdict.keys())
+    plot.line(source = src,x = keylist[0], y = [i for i in keylist[1:]])
+    show(plot)
+
+
+
